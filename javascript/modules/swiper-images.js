b984a8ba1a60b899202c54f337fa71ae53528c2b
xenolito <oscar.rey.tajes@gmail.com>
2024-08-30 09:41:03 +0200

    updated for production

--- javascript/modules/swiper-images.js
+++ javascript/modules/swiper-images.js
@@ -27,21 +27,26 @@ import Swiper from 'swiper'
 import { Pagination, Autoplay, EffectFade } from 'swiper/modules'
 
 document.addEventListener('DOMContentLoaded', () => {
-	document.querySelectorAll('.slider-bg').forEach(el => {
-		let autoplayDelay = el.dataset?.slider_autoplay_delay
+	const swipers = document.querySelectorAll('.swiper-img-fade')
 
-		const swiperBg = new Swiper('.slider-bg', {
+	if (!swipers.length) return
+
+	swipers.forEach(el => {
+		let autoplayDelay = el.dataset?.slider_autoplay_delay
+		// console.log(autoplayDelay)
+
+		const swiperBg = new Swiper('.swiper-img-fade', {
 			modules: [Pagination, Autoplay, EffectFade],
 			loop: true,
 			// effect: 'flip',
 			effect: 'fade',
-			fadeEffect: {
-				crossFade: true,
-			},
+			// fadeEffect: {
+			// 	crossFade: true,
+			// },
 			// flipEffect: {
 			// 	slideShadows: false,
 			// },
-			slidesPerView: 1,
+			// slidesPerView: 1,
 			spaceBetween: 0,
 			// centeredSlides: true,
 			preventClicks: false,
