395ad254570b99566b9189e7fa05fb7d4209a4d3
xenolito <oscar.rey.tajes@gmail.com>
2026-08-30 11:27:44 +0200

    Removed link rel='profile' from header

--- theme/header.php
+++ theme/header.php
@@ -18,7 +18,6 @@
 <head>
 	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
 	<?php wp_head(); ?>
 </head>
 
